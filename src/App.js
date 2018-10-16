7c6384c857a29d3769b298c1baa75c4e0f672e21
Matt Winzer <matt.winzer@gmail.com>
2018-10-16 10:52:22 -0600

    added Footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 
 // Components
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Footer />
       </div>
     );
   }
